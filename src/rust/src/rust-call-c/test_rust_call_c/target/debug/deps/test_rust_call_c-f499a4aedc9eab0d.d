01e647999d2e96d886f55f15d0e968c8c8c6a059
jason_yao <jason_yao>
2022-04-09 11:03:14 +0800

    update

--- src/rust/src/rust-call-c/test_rust_call_c/target/debug/deps/test_rust_call_c-f499a4aedc9eab0d.d
+++
@@ -1,5 +0,0 @@
-/home/shihyu/github/jason_note/src/rust/src/rust-call-c/test_rust_call_c/target/debug/deps/test_rust_call_c-f499a4aedc9eab0d.rmeta: src/main.rs
-
-/home/shihyu/github/jason_note/src/rust/src/rust-call-c/test_rust_call_c/target/debug/deps/test_rust_call_c-f499a4aedc9eab0d.d: src/main.rs
-
-src/main.rs:
--- src/rust/src/rust-call-c/test_rust_call_c/target/debug/deps/test_rust_call_c-fc0fcc51c22938c6.d
+++
@@ -1,5 +0,0 @@
-/home/shihyu/github/jason_note/src/rust/src/rust-call-c/test_rust_call_c/target/debug/deps/test_rust_call_c-fc0fcc51c22938c6.rmeta: src/main.rs
-
-/home/shihyu/github/jason_note/src/rust/src/rust-call-c/test_rust_call_c/target/debug/deps/test_rust_call_c-fc0fcc51c22938c6.d: src/main.rs
-
-src/main.rs:
