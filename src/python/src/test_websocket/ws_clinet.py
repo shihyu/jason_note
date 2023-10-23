5a4e7cb29c8add607c600e54c105ee016b981670
jason_yao <jason_yao>
2023-10-24 01:12:33 +0800

    update

--- src/python/src/test_websocket/ws_clinet.py
+++ src/python/src/test_websocket/ws_clinet.py
@@ -1,32 +1,21 @@
 import websocket
+import time
 
+# WebSocket服务器URL
+websocket_url = "ws://localhost:8888/websocket"
 
-def on_message(ws, message):
-    print(f"Received message from server: {message}")
+while True:
+    try:
+        ws = websocket.WebSocket()
+        ws.connect(websocket_url)
+        time.sleep(1)
 
+        while True:
+            message = ws.recv()
+            print(f"Received message from server: {message}")
 
-def on_error(ws, error):
-    print(f"Error: {error}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-
-def on_close(ws, close_status_code, close_msg):
-    print("WebSocket connection closed")
-
-
-if __name__ == "__main__":
-    # 请替换以下 URL 为你的 WebSocket 服务器的 URL
-    websocket_url = "ws://localhost:8888/websocket"
-    ws = websocket.WebSocketApp(
-        websocket_url, on_message=on_message, on_error=on_error, on_close=on_close
-    )
-
-    # 启动 WebSocket 连接
-    ws.run_forever()
-
-    # 循环发送消息给服务器
-    while True:
-        message = input("Enter a message to send to the server (or 'exit' to quit): ")
-        if message == "exit":
-            break
-        else:
-            ws.send(message)
+    # 在连接断开后等待一段时间再尝试重新连接
+    time.sleep(5)
