ae855fd692fc4ad3b487a4558be9588a0624c45e
jason_yao <jason_yao>
2023-10-24 01:11:56 +0800

    update

--- src/python/src/test_websocket/ws_server.py
+++ src/python/src/test_websocket/ws_server.py
@@ -1,11 +1,15 @@
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
+from tornado.ioloop import PeriodicCallback
 
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         print("WebSocket connection opened")
+        WebSocketHandler.connections.add(
+            self
+        )  # Add the new client to the set of connections
 
     def on_message(self, message):
         print(f"Received message from client: {message}")
@@ -13,6 +17,19 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
     def on_close(self):
         print("WebSocket connection closed")
+        WebSocketHandler.connections.remove(
+            self
+        )  # Remove the client when they disconnect
+
+
+def push_hello():
+    for handler in WebSocketHandler.connections:
+        handler.write_message(
+            "Hello from server"
+        )  # Send a message to each connected client
+
+
+WebSocketHandler.connections = set()
 
 
 def make_app():
@@ -27,4 +44,9 @@ if __name__ == "__main__":
     app = make_app()
     app.listen(8888)
     print("WebSocket server is running on port 8888")
+
+    # Every second, push messages to connected clients
+    callback = PeriodicCallback(push_hello, 1000)
+    callback.start()
+
     tornado.ioloop.IOLoop.current().start()
