ab05f8caca64d6f9b8fc7ce571e730fddd407ee5
jason_yao <jason_yao>
2023-10-24 00:58:53 +0800

    update

--- src/python/src/test_websocket/ws_clinet.py
+++ src/python/src/test_websocket/ws_clinet.py
@@ -1,7 +1,7 @@
 import websocket
 
 def on_message(ws, message):
-    print(f"Received message: {message}")
+    print(f"Received message from server: {message}")
 
 def on_error(ws, error):
     print(f"Error: {error}")
@@ -14,5 +14,14 @@ if __name__ == "__main__":
     websocket_url = "ws://localhost:8888/websocket"
     ws = websocket.WebSocketApp(websocket_url, on_message=on_message, on_error=on_error, on_close=on_close)
 
+    # 启动 WebSocket 连接
     ws.run_forever()
+
+    # 循环发送消息给服务器
+    while True:
+        message = input("Enter a message to send to the server (or 'exit' to quit): ")
+        if message == "exit":
+            break
+        else:
+            ws.send(message)
 
