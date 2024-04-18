0cff691da12b943ea4984f6042d6744eee3abba4
jason_yao <jason_yao>
2024-04-18 23:17:35 +0800

    Update documentation

---
+++ rust/src/pyo3_polars_extension/target/release/build/proc-macro2-25bc292bafa77101/out/proc_macro2.d
@@ -0,0 +1,7 @@
+/home/shihyu/github/jason_note/src/rust/src/pyo3_polars_extension/target/release/build/proc-macro2-25bc292bafa77101/out/libproc_macro2.rmeta: build/probe.rs
+
+/home/shihyu/github/jason_note/src/rust/src/pyo3_polars_extension/target/release/build/proc-macro2-25bc292bafa77101/out/proc_macro2.d: build/probe.rs
+
+build/probe.rs:
+
+# env-dep:RUSTC_BOOTSTRAP
---
+++ rust/src/pyo3_polars_extension/target/release/deps/autocfg-a213f286d5dd51ba.d
@@ -0,0 +1,9 @@
+/home/shihyu/github/jason_note/src/rust/src/pyo3_polars_extension/target/release/deps/libautocfg-a213f286d5dd51ba.rmeta: /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.2.0/src/lib.rs /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.2.0/src/error.rs /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.2.0/src/version.rs
+
+/home/shihyu/github/jason_note/src/rust/src/pyo3_polars_extension/target/release/deps/libautocfg-a213f286d5dd51ba.rlib: /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.2.0/src/lib.rs /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.2.0/src/error.rs /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.2.0/src/version.rs
+
+/home/shihyu/github/jason_note/src/rust/src/pyo3_polars_extension/target/release/deps/autocfg-a213f286d5dd51ba.d: /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.2.0/src/lib.rs /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.2.0/src/error.rs /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.2.0/src/version.rs
+
+/home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.2.0/src/lib.rs:
+/home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.2.0/src/error.rs:
+/home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.2.0/src/version.rs:
