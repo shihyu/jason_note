89d448ed56c1f6abef49409f886bde32d307dfcd
Jason <yaoshihyu@gmail.com>
2024-04-26 11:40:19 +0800

    update

--- src/python/data/cython_cdef_vs_def/main.py
+++ src/python/data/cython_cdef_vs_def/main.py
@@ -3,17 +3,17 @@ import run_python
 import time
 
 # num = 1000000
-num = 100000
+num = 100
 
 start = time.time()
-run_python.test(num)
+print(run_python.test(num))
 end = time.time()
 py_time = end - start
 print(f"Python time: {py_time}")
 
 
 start = time.time()
-run_cython.test(num)
+print(run_cython.test(num))
 end = time.time()
 cy_time = end - start
 print(f"Cython time: {cy_time}")
