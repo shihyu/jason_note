91d58258f382b041d79ff93f929fb9126fdc954a
jason_yao <jason_yao>
2024-06-21 22:01:22 +0800

    update

---
+++ src/python/data/socket/單向/server.py
@@ -0,0 +1,24 @@
+import socket
+
+
+def server_program():
+    host = "127.0.0.1"  # localhost
+    port = 5000  # port to listen on
+
+    server_socket = socket.socket()
+    server_socket.bind((host, port))
+    server_socket.listen(1)
+    conn, address = server_socket.accept()
+    print(f"Connection from: {address}")
+
+    while True:
+        data = conn.recv(1024).decode()
+        if not data:
+            break
+        print(f"Received from client: {data}")
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    server_program()
