8a07c2c48f9883f87e75d5f7738fd89e17f1c118
Jason <yaoshihyu@gmail.com>
2024-06-28 11:19:17 +0800

    Update documentation

--- rust/src/pyo3_polars_extension/target/release/build/memoffset-82952ded4d53d751/build_script_build-82952ded4d53d751.d
+++
@@ -1,5 +0,0 @@
-/home/shihyu/github/jason_note/src/rust/src/pyo3_polars_extension/target/release/build/memoffset-82952ded4d53d751/build_script_build-82952ded4d53d751: /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/memoffset-0.8.0/build.rs
-
-/home/shihyu/github/jason_note/src/rust/src/pyo3_polars_extension/target/release/build/memoffset-82952ded4d53d751/build_script_build-82952ded4d53d751.d: /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/memoffset-0.8.0/build.rs
-
-/home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/memoffset-0.8.0/build.rs:
