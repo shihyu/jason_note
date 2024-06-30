519d3feb6090c67c26625a6c77a662d4f6943f32
jason_yao <jason_yao>
2024-06-30 10:02:46 +0800

    Update documentation

---
+++ rust/src/pyo3_polars_extension/target/release/build/syn-741dbc15d8a57f8a/build_script_build-741dbc15d8a57f8a.d
@@ -0,0 +1,5 @@
+/home/shihyu/github/jason_note/src/rust/src/pyo3_polars_extension/target/release/build/syn-741dbc15d8a57f8a/build_script_build-741dbc15d8a57f8a: /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/syn-1.0.109/build.rs
+
+/home/shihyu/github/jason_note/src/rust/src/pyo3_polars_extension/target/release/build/syn-741dbc15d8a57f8a/build_script_build-741dbc15d8a57f8a.d: /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/syn-1.0.109/build.rs
+
+/home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/syn-1.0.109/build.rs:
---
+++ rust/src/pyo3_polars_extension/target/release/deps/python_rust.d
@@ -0,0 +1,5 @@
+/home/shihyu/github/jason_note/src/rust/src/pyo3_polars_extension/target/release/deps/libpython_rust.so: src/lib.rs
+
+/home/shihyu/github/jason_note/src/rust/src/pyo3_polars_extension/target/release/deps/python_rust.d: src/lib.rs
+
+src/lib.rs:
