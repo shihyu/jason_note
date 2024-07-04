68d682d83b7f9e36d3004fac1a08a0f3d9b84166
Jason <yaoshihyu@gmail.com>
2024-07-04 16:17:08 +0800

    Update documentation

--- rust/src/pyo3_polars_extension/target/release/deps/once_cell-1d81694c96054008.d
+++
@@ -1,9 +0,0 @@
-/home/shihyu/github/jason_note/src/rust/src/pyo3_polars_extension/target/release/deps/libonce_cell-1d81694c96054008.rmeta: /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/lib.rs /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/imp_std.rs /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/race.rs
-
-/home/shihyu/github/jason_note/src/rust/src/pyo3_polars_extension/target/release/deps/libonce_cell-1d81694c96054008.rlib: /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/lib.rs /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/imp_std.rs /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/race.rs
-
-/home/shihyu/github/jason_note/src/rust/src/pyo3_polars_extension/target/release/deps/once_cell-1d81694c96054008.d: /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/lib.rs /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/imp_std.rs /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/race.rs
-
-/home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/lib.rs:
-/home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/imp_std.rs:
-/home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/once_cell-1.19.0/src/race.rs:
