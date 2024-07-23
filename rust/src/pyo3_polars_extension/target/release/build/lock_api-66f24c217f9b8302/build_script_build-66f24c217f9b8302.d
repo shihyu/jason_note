6e1bb6e136c6838ec2dd16fe070c217c2e9498b3
Jason <yaoshihyu@gmail.com>
2024-07-23 15:55:18 +0800

    Update documentation

--- rust/src/pyo3_polars_extension/target/release/build/lock_api-66f24c217f9b8302/build_script_build-66f24c217f9b8302.d
+++
@@ -1,5 +0,0 @@
-/home/shihyu/github/jason_note/src/rust/src/pyo3_polars_extension/target/release/build/lock_api-66f24c217f9b8302/build_script_build-66f24c217f9b8302: /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lock_api-0.4.11/build.rs
-
-/home/shihyu/github/jason_note/src/rust/src/pyo3_polars_extension/target/release/build/lock_api-66f24c217f9b8302/build_script_build-66f24c217f9b8302.d: /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lock_api-0.4.11/build.rs
-
-/home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lock_api-0.4.11/build.rs:
--- rust/src/pyo3_polars_extension/target/release/deps/pyo3_macros-e0042b8a12899655.d
+++
@@ -1,5 +0,0 @@
-/home/shihyu/github/jason_note/src/rust/src/pyo3_polars_extension/target/release/deps/libpyo3_macros-e0042b8a12899655.so: /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pyo3-macros-0.18.3/src/lib.rs
-
-/home/shihyu/github/jason_note/src/rust/src/pyo3_polars_extension/target/release/deps/pyo3_macros-e0042b8a12899655.d: /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pyo3-macros-0.18.3/src/lib.rs
-
-/home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pyo3-macros-0.18.3/src/lib.rs:
