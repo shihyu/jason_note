0346fd51cea269b04f103639fbffd39493520447
jason_yao <jason_yao>
2024-08-03 22:10:26 +0800

    update

--- src/ml/data/yolov8/train.py
+++ src/ml/data/yolov8/train.py
@@ -4,13 +4,17 @@ import os
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
-
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    # 獲取當前腳本的目錄
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    # 構建 data.yaml 的絕對路徑
+    data_yaml_path = os.path.join(current_dir, "data.yaml")
+    print(data_yaml_path)
+    input()
 
     # load model
     model = YOLO("models/yolov8n.pt")
     results = model.train(
-        data="dataset.yaml",
+        data=data_yaml_path,
         imgsz=256,
         epochs=30,
         batch=10,
