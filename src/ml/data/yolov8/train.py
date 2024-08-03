7ffbcb463f0ac7de18c812aca8d9a6e170c0fd62
jason_yao <jason_yao>
2024-08-03 22:13:47 +0800

    update

--- src/ml/data/yolov8/train.py
+++ src/ml/data/yolov8/train.py
@@ -8,8 +8,6 @@ if __name__ == "__main__":
     current_dir = os.path.dirname(os.path.abspath(__file__))
     # 構建 data.yaml 的絕對路徑
     data_yaml_path = os.path.join(current_dir, "data.yaml")
-    print(data_yaml_path)
-    input()
 
     # load model
     model = YOLO("models/yolov8n.pt")
