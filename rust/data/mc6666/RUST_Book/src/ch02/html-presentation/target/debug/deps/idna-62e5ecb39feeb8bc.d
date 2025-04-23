3f8cba55f60a2962760ab249de5d5fccdd57a9c8
Jason <yaoshihyu@gmail.com>
2025-04-23 13:59:21 +0800

    Update documentation

--- rust/data/mc6666/RUST_Book/src/ch02/html-presentation/target/debug/deps/idna-62e5ecb39feeb8bc.d
+++
@@ -1,8 +0,0 @@
-/home/shihyu/github/jason_note/src/rust/data/mc6666/RUST_Book/src/ch02/html-presentation/target/debug/deps/libidna-62e5ecb39feeb8bc.rmeta: /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/idna-0.5.0/src/lib.rs /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/idna-0.5.0/src/punycode.rs /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/idna-0.5.0/src/uts46.rs /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/idna-0.5.0/src/uts46_mapping_table.rs
-
-/home/shihyu/github/jason_note/src/rust/data/mc6666/RUST_Book/src/ch02/html-presentation/target/debug/deps/idna-62e5ecb39feeb8bc.d: /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/idna-0.5.0/src/lib.rs /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/idna-0.5.0/src/punycode.rs /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/idna-0.5.0/src/uts46.rs /home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/idna-0.5.0/src/uts46_mapping_table.rs
-
-/home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/idna-0.5.0/src/lib.rs:
-/home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/idna-0.5.0/src/punycode.rs:
-/home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/idna-0.5.0/src/uts46.rs:
-/home/shihyu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/idna-0.5.0/src/uts46_mapping_table.rs:
