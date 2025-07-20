7b7c5456d744ed9093e0ee83e02d28368e088162
jason_yao <yaoshihyu@gmail.com>
2025-07-20 21:56:34 +0800

    Update documentation

---
+++ os/data/ChCore/build/CMakeFiles/userland-clean-incbin.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/userland-clean-incbin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/userland-clean-incbin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ os/data/ChCore/libchcore/_build/CMakeFiles/chcore_objects.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/chos/libchcore/src/capability/capability.c" "/chos/libchcore/_build/CMakeFiles/chcore_objects.dir/src/capability/capability.c.o"
+  "/chos/libchcore/src/console/base.c" "/chos/libchcore/_build/CMakeFiles/chcore_objects.dir/src/console/base.c.o"
+  "/chos/libchcore/src/console/printf.c" "/chos/libchcore/_build/CMakeFiles/chcore_objects.dir/src/console/printf.c.o"
+  "/chos/libchcore/src/env/__libchcore_init.c" "/chos/libchcore/_build/CMakeFiles/chcore_objects.dir/src/env/__libchcore_init.c.o"
+  "/chos/libchcore/src/fs/fakefs.c" "/chos/libchcore/_build/CMakeFiles/chcore_objects.dir/src/fs/fakefs.c.o"
+  "/chos/libchcore/src/fs/fsm.c" "/chos/libchcore/_build/CMakeFiles/chcore_objects.dir/src/fs/fsm.c.o"
+  "/chos/libchcore/src/fs/tmpfs.c" "/chos/libchcore/_build/CMakeFiles/chcore_objects.dir/src/fs/tmpfs.c.o"
+  "/chos/libchcore/src/internal/server_caps.c" "/chos/libchcore/_build/CMakeFiles/chcore_objects.dir/src/internal/server_caps.c.o"
+  "/chos/libchcore/src/ipc/ipc.c" "/chos/libchcore/_build/CMakeFiles/chcore_objects.dir/src/ipc/ipc.c.o"
+  "/chos/libchcore/src/libc/malloc.c" "/chos/libchcore/_build/CMakeFiles/chcore_objects.dir/src/libc/malloc.c.o"
+  "/chos/libchcore/src/libc/string.c" "/chos/libchcore/_build/CMakeFiles/chcore_objects.dir/src/libc/string.c.o"
+  "/chos/libchcore/src/memory/pmo.c" "/chos/libchcore/_build/CMakeFiles/chcore_objects.dir/src/memory/pmo.c.o"
+  "/chos/libchcore/src/memory/vaddr.c" "/chos/libchcore/_build/CMakeFiles/chcore_objects.dir/src/memory/vaddr.c.o"
+  "/chos/libchcore/src/procm/spawn.c" "/chos/libchcore/_build/CMakeFiles/chcore_objects.dir/src/procm/spawn.c.o"
+  "/chos/libchcore/src/sync/spin.c" "/chos/libchcore/_build/CMakeFiles/chcore_objects.dir/src/sync/spin.c.o"
+  "/chos/libchcore/src/thread/create.c" "/chos/libchcore/_build/CMakeFiles/chcore_objects.dir/src/thread/create.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CHCORE"
+  "CHCORE_ARCH=\"aarch64\""
+  "CHCORE_ARCH_AARCH64"
+  "CHCORE_CROSS_COMPILE=\"aarch64-linux-gnu-\""
+  "CHCORE_KERNEL_TEST"
+  "CHCORE_PLAT=\"raspi3\""
+  "CHCORE_PLAT_RASPI3"
+  "CHCORE_ROOT_PROGRAM=\"procm.srv\""
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include/arch/aarch64"
+  "../include"
+  "../include/libc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
