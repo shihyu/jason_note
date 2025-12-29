a72df6adaacc88af094bef6fa415e8e9330fcd12
jason_yao <jason_yao>
2025-12-29 15:05:02 +0800

    Update documentation

---
+++ gdb/data/llama.cpp/build/tests/CMakeFiles/test-regex-partial.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for test-regex-partial.
+# This may be replaced when dependencies are built.
---
+++ gdb/data/llama.cpp/build/tools/quantize/CMakeFiles/llama-quantize.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../bin/llama-quantize"
+  "../../bin/llama-quantize.pdb"
+  "CMakeFiles/llama-quantize.dir/quantize.cpp.o"
+  "CMakeFiles/llama-quantize.dir/quantize.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/llama-quantize.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
