c340dbc4dfbec88fba684c646e8bbfaa1eb705ff
jason_yao <jason_yao>
2026-01-15 16:00:01 +0800

    Update documentation

---
+++ gdb/data/llama.cpp/build/examples/embedding/CMakeFiles/llama-embedding.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../bin/llama-embedding"
+  "../../bin/llama-embedding.pdb"
+  "CMakeFiles/llama-embedding.dir/embedding.cpp.o"
+  "CMakeFiles/llama-embedding.dir/embedding.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/llama-embedding.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gdb/data/llama.cpp/build/examples/passkey/CMakeFiles/llama-passkey.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for llama-passkey.
+# This may be replaced when dependencies are built.
