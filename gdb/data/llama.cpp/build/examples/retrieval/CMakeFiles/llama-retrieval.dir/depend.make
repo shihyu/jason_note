d77a6087e6b0697b68b4a9566b5f86ab0eefff1d
jason_yao <jason_yao>
2026-01-19 23:50:02 +0800

    Update documentation

--- gdb/data/llama.cpp/build/examples/retrieval/CMakeFiles/llama-retrieval.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for llama-retrieval.
-# This may be replaced when dependencies are built.
--- gdb/data/llama.cpp/build/tools/llama-bench/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shihyu/github/jason_note/src/gdb/data/llama.cpp/tools/llama-bench
-# Build directory: /home/shihyu/github/jason_note/src/gdb/data/llama.cpp/build/tools/llama-bench
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
