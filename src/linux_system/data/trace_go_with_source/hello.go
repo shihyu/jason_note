729fd7968baa67f63f82114eb3911b14963f881e
jason_yao <jason_yao>
2026-03-18 22:06:16 +0800

    update

--- src/linux_system/data/trace_go_with_source/hello.go
+++ src/linux_system/data/trace_go_with_source/hello.go
@@ -4,6 +4,14 @@ import (
 	"fmt"
 )
 
+func swap(a, b int) (int, int) {
+	return b, a
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	a, b := 1, 2
+	fmt.Printf("Before swap: a = %d, b = %d\n", a, b)
+	// fmt.Println("Hello, World!")
+	a, b = swap(a, b)
+	fmt.Printf("After swap: a = %d, b = %d\n", a, b)
 }
