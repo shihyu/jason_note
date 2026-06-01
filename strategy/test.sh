b813413debed98f2ef84f48065e7cb646bd476bd
jason_yao <jason_yao>
2026-06-02 03:31:29 +0800

    Update documentation

--- strategy/test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-echo "==> system tests"
-PYTHONDONTWRITEBYTECODE=1 PYTEST_DISABLE_PLUGIN_AUTOLOAD=1 python3 -m pytest -p no:cacheprovider tests/system
-echo "==> all tests passed"
